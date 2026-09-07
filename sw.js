da479659369a6c11e46b4daeafe184681a05b891
SrRazum <50208624+SrRazum@users.noreply.github.com>
2026-09-07 15:50:03 -0300

    Atualiza identificacao do cache para V1.14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "financeiro-v1.13";
+const APP_VERSION = "financeiro-v1.14";
 const CACHE_NAME = `meu-controle-${APP_VERSION}`;
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./logo.png"];
 self.addEventListener("install", event => {
